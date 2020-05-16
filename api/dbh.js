9dd295afb63e80ed1179bae8362f38c6049dc05b
Garrick Crouch <services@localpathcomputing.com>
2020-05-16 12:21:52 -0500

    fix:account reg tld

--- api/dbh.js
+++ api/dbh.js
@@ -1,19 +1,3 @@
-/* const conn = require('mysql').createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME,
-}); */
-
-/* conn.connect((err) => {
-    if (err) {
-      console.error('error connecting: ' + err.stack)
-      return
-    }
-})
- 
-module.exports = { conn } */
-
 let conn = null;
 function handleDisconnect() {
   conn = require('mysql').createConnection({
