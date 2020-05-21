dd748f8f074be70f8e6fe063b504a10fae4b3498
Garrick Crouch <services@localpathcomputing.com>
2020-05-20 20:05:37 -0500

    fix:init.sql

--- init.sql
+++ init.sql
@@ -1,17 +1,12 @@
-DROP DATABASE IF EXISTS `go_do`;
-CREATE DATABASE `go_do`;
-
-use `go_do`;
-
 CREATE TABLE `users` (
     `id` int(11) NOT NULL AUTO_INCREMENT,
     `first_name` varchar(255) NOT NULL,
     `email` varchar(255) NOT NULL,
     `password` varchar(255) NOT NULL,
-    `verified_at` DATETIME DEFAULT NULL,
-    `created_at` DATETIME DEFAULT CURRENT_TIMESTAMP,
-    `updated_at` DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    `deleted_at` DATETIME DEFAULT NULL,
+    `verified_at` TIMESTAMP NULL,
+    `created_at` TIMESTAMP NULL,
+    `updated_at` TIMESTAMP NULL ON UPDATE CURRENT_TIMESTAMP,
+    `deleted_at` TIMESTAMP NULL,
     PRIMARY KEY (id),
     CONSTRAINT `email` UNIQUE(email)
 );
@@ -22,9 +17,9 @@ CREATE TABLE `lists` (
     `user_id` int(11) NOT NULL,
     `title` varchar(255),
     `description` varchar(255),
-    `created_at` DATETIME DEFAULT CURRENT_TIMESTAMP,
-    `updated_at` DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    `deleted_at` DATETIME DEFAULT NULL,
+    `created_at` TIMESTAMP NULL,
+    `updated_at` TIMESTAMP NULL ON UPDATE CURRENT_TIMESTAMP,
+    `deleted_at` TIMESTAMP NULL,
     PRIMARY KEY (id),
     FOREIGN KEY (user_id) REFERENCES users(id),
     CONSTRAINT `guid` UNIQUE(guid)
@@ -36,9 +31,9 @@ CREATE TABLE `items` (
     `guid` varchar(255) NOT NULL,
     `title` varchar(255),
     `description` varchar(255),
-    `created_at` DATETIME DEFAULT CURRENT_TIMESTAMP,
-    `updated_at` DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    `deleted_at` DATETIME DEFAULT NULL,
+    `created_at` TIMESTAMP NULL,
+    `updated_at` TIMESTAMP NULL ON UPDATE CURRENT_TIMESTAMP,
+    `deleted_at` TIMESTAMP NULL,
     PRIMARY KEY (id),
     FOREIGN KEY (list_id) REFERENCES lists(id),
     CONSTRAINT `guid` UNIQUE(guid)
@@ -47,10 +42,10 @@ CREATE TABLE `items` (
 CREATE TABLE `notifications` (
     `id` int(11) NOT NULL AUTO_INCREMENT,
     `list_id` int(11),
-    `notifies_on` DATETIME DEFAULT CURRENT_TIMESTAMP,
-    `created_at` DATETIME DEFAULT CURRENT_TIMESTAMP,
-    `updated_at` DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    `deleted_at` DATETIME DEFAULT NULL,
+    `notifies_on` DATETIME NULL,
+    `created_at` TIMESTAMP NULL,
+    `updated_at` TIMESTAMP NULL ON UPDATE CURRENT_TIMESTAMP,
+    `deleted_at` TIMESTAMP NULL,
     PRIMARY KEY (id),
     FOREIGN KEY (list_id) REFERENCES lists(id)
 );
@@ -65,12 +60,12 @@ CREATE TABLE `pending_registrations` (
     FOREIGN KEY (user_id) REFERENCES users(id)
 );
 
-CREATE TABLE `web_tokens` (
+CREATE TABLE `password_resets` (
     `id` int(11) NOT NULL AUTO_INCREMENT,
     `user_id` int(11),
-    `web_token` varchar(255),
-    `token_expires` bigint UNSIGNED,
-    `refresh_token` varchar(255),
+    `verification_link` varchar(255),
+    `created_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    `deleted_at` DATETIME DEFAULT NULL,
     PRIMARY KEY (id),
     FOREIGN KEY (user_id) REFERENCES users(id)
 );
