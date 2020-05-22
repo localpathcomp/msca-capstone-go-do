b2dc9dc3ae6dce5a46d108d9d4f20d3df5eeab55
Garrick Crouch <services@localpathcomputing.com>
2020-05-21 20:12:47 -0500

    fix: use cwd for routing error

--- api/index.js
+++ api/index.js
@@ -1,10 +1,11 @@
+const process = require('process')
 const path = require('path')
 
 const index = (req, res) => {
     if (process.env.NODE_ENV === 'development') {
         res.status(200).redirect('http://localhost:3000')
     } else {
-        res.sendFile(path.join(__dirname + '/../../build', 'index.html'))
+        res.sendFile(path.join(process.cwd(), 'build', 'index.html'))
     }
 }
 
