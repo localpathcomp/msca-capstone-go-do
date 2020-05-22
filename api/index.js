815968aa0f50f711020b698b2dfb792223b15f13
Garrick Crouch <services@localpathcomputing.com>
2020-05-21 19:38:55 -0500

    fix: * route error

--- api/index.js
+++ api/index.js
@@ -4,7 +4,7 @@ const index = (req, res) => {
     if (process.env.NODE_ENV === 'development') {
         res.status(200).redirect('http://localhost:3000')
     } else {
-        res.sendFile(path.join('../../build', 'index.html'))
+        res.sendFile(path.join(__dirname + '/../../build', 'index.html'))
     }
 }
 
